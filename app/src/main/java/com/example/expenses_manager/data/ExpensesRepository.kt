1b0453c086958dc4c82552344256ada22dba648d
Vladyslav <vladlit.vinenko2909@gmail.com>
2025-12-04 23:43:29 +0200

    Created Expense.kt, ExpenseDao.kt, AppDatabase.kt, ExpensesRepository.kt for repo layer

---
+++ app/src/main/java/com/example/expenses_manager/data/ExpensesRepository.kt
@@ -0,0 +1,15 @@
+package com.example.expenses_manager.data
+
+import kotlinx.coroutines.flow.Flow
+
+class ExpensesRepository(private val expenseDao: ExpenseDao) {
+    val allExpenses: Flow<List<Expense>> = expenseDao.getAllExpenses()
+
+    suspend fun insertExpense(expense: Expense) {
+        expenseDao.insertExpense(expense)
+    }
+
+    suspend fun deleteExpense(expense: Expense) {
+        expenseDao.deleteExpense(expense)
+    }
+}
