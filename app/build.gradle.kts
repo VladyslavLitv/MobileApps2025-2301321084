5016113b8df315873ed139afc50d3061830d6dd0
Vladyslav <vladlit.vinenko2909@gmail.com>
2025-12-05 00:49:06 +0200

    Updated libs.versions.toml and build.gradle.kts for new test dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,12 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
+    // Testing
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.kotlinx.coroutines.test)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
